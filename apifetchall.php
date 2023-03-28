d6f8889d4d512d01f5f6ca3803d5c80d5a417cde
Sagar Rajput <71219774+astrogeeksagar@users.noreply.github.com>
2023-03-28 14:43:37 +0530

    Fetch all record from the table

---
+++ apifetchall.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+include 'connect.php';
+
+$PDO = Database::connect();
+$sql = "select * from studentdata";
+$stmt = $PDO->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (count($result) > 0) {
+    $data = $result;
+
+  echo json_encode($data);
+} else {
+  echo json_encode(['msg' => 'No Data!', 'status' => false]);
+}
+
+Database::disconnect();
+?>
